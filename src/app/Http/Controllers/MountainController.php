ffa48032e82c968fb7c43b26b317a8653f65af47
railstar113 <kodama113@gmail.com>
2024-02-27 22:27:21 +0900

    天気ページのPCレイアウト調整

--- src/app/Http/Controllers/MountainController.php
+++ src/app/Http/Controllers/MountainController.php
@@ -8,24 +8,93 @@ use Illuminate\Support\Facades\Http;
 
 class MountainController extends Controller
 {
-    public function weather(Mountain $mountain)
-    {
-        // $mountain = Mountain::findOrFail($id);
+  public function weather(Mountain $mountain)
+  {
+    // OpenWeatherAPIで天気データ取得
+    $lat = $mountain->latitude;
+    $lon = $mountain->longitude;
+    $apiKey = config('api.key');
+    $lang = config('api.lang');
+    $response = Http::get("https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&exclude=minutely&appid=$apiKey&units=metric&lang=$lang");
+    $weatherData = $response->json();
 
-        $lat = $mountain->latitude;
-        $lon = $mountain->longitude;
-        $apiKey = config('api.key');
-        $lang = config('api.lang');
-        $response = Http::get("https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&exclude=minutely&appid=$apiKey&units=metric&lang=$lang");
-        $weatherData = $response->json();
+    // 天気データ整形ここから
+    // 地域データのみ取得
+    $weatherCityData = $weatherData['city'];
+    foreach ($weatherData['list'] as $i => $weather3h) :
+      // 風向きを数値から日本語に変換
+      $degs = ['北', '北北東', '北東', '東北東', '東', '東南東', '南東', '南南東', '南', '南南西', '南西', '西南西', '西北西', '北西', '北北西', '北', '北北東'];
+      $degIndex = round($weather3h['wind']['deg'] / 22.5);
+      $weather3h['wind']['deg'] = $degs[$degIndex];
+      $weatherData['list'][$i]['wind']['direction'] = $degs[$degIndex];
+      
+      // 曜日を日本語に変換
+      $days = ['日', '月', '火', '水', '木', '金', '土'];
+      $dayIndex = date('w', $weather3h['dt']);
+      $weatherData['list'][$i]['dt_day'] = $days[$dayIndex];
+      
+      // 現在の天気データのみ取得
+      $weatherNow = $weatherData['list'][0];
+      // 現在の次の時間以降の天気データ取得（3時間毎）
+      if ($i === 1) :
+        $nextHour = date('H', $weather3h['dt']);
+        $emptyTag = '<td class="empty"></td>';
+        $emptyTags = [];
+        switch ($nextHour) {
+          case ('00'):
+            $emptyTags['first'] = '';
+            $emptyTags['last'] = str_repeat($emptyTag, 1);
+            $breakIndex = 9;
+            break;
+          case ('03'):
+            $emptyTags['first'] = str_repeat($emptyTag, 1);
+            $emptyTags['last'] = '';
+            $breakIndex = 8;
+            break;
+          case ('06'):
+            $emptyTags['first'] = str_repeat($emptyTag, 2);
+            $emptyTags['last'] = str_repeat($emptyTag, 7);
+            $breakIndex = 7;
+            break;
+          case ('09'):
+            $emptyTags['first'] = str_repeat($emptyTag, 3);
+            $emptyTags['last'] = str_repeat($emptyTag, 6);
+            $breakIndex = 6;
+            break;
+          case ('12'):
+            $emptyTags['first'] = str_repeat($emptyTag, 4);
+            $emptyTags['last'] = str_repeat($emptyTag, 5);
+            $breakIndex = 5;
+            break;
+          case ('15'):
+            $emptyTags['first'] = str_repeat($emptyTag, 5);
+            $emptyTags['last'] = str_repeat($emptyTag, 4);
+            $breakIndex = 4;
+            break;
+          case ('18'):
+            $emptyTags['first'] = str_repeat($emptyTag, 6);
+            $emptyTags['last'] = str_repeat($emptyTag, 3);
+            $breakIndex = 3;
+            break;
+          case ('21'):
+            $emptyTags['first'] = str_repeat($emptyTag, 7);
+            $emptyTags['last'] = str_repeat($emptyTag, 2);
+            $breakIndex = 2;
+            break;
+          default:
+            break;
+        }
+      endif;
+    endforeach;
+    $weather3hData[] = array_slice($weatherData['list'], 1, $breakIndex - 1);
+    for ($i = $breakIndex; $i <= count($weatherData['list']); $i += 8) :
+      $every8Pieces = array_slice($weatherData['list'], $i, 8);
+      if (count($every8Pieces) !== 0) :
+        $weather3hData[] = $every8Pieces;
+      endif;
+    endfor;
+    // 天気データ整形ここまで
 
-        $deg_ja = ['北', '北北東', '北東', '東北東', '東', '東南東', '南東', '南南東', '南', '南南西', '南西', '西南西', '西北西', '北西', '北北西', '北', '北北東'];
-        $degData = [];
-        foreach ($weatherData['list'] as $weather_3hours) :
-            $degIndex = round($weather_3hours['wind']['deg'] / 22.5);
-            $degData[] = $deg_ja[$degIndex];
-        endforeach;
-
-        return view('mountains.weather', compact('mountain', 'weatherData', 'degData'));
-    }
+    return view('mountains.weather', compact('mountain', 'weatherCityData', 'weatherNow', 'weather3hData', 'emptyTags'));
+  }
 }
--- src/resources/views/mountains/weather.blade.php
+++ src/resources/views/mountains/weather.blade.php
@@ -3,216 +3,120 @@
     {{ $mountain->name }} - 天気予報
   </x-slot>
 
-  <h1>{{ $mountain->name }} - {{ $weatherData['city']['name'] }}付近の<span class="i_block">天気予報</span></h1>
+  <h1>{{ $mountain->name }} - {{ $weatherCityData['name'] }}付近の<span class="i_block">天気予報</span></h1>
   <div class="container">
-    <p class="back_link">&laquo; <a href="{{ route('prefectures.show', $mountain->prefecture) }}">{{ $mountain->prefecture->name }}の山一覧</a></a></p>
-    
-    {{-- レイアウト調整用ここから --}}
+    <p class="back_link">&laquo; <a
+        href="{{ route('prefectures.show', $mountain->prefecture) }}">{{ $mountain->prefecture->name }}の山一覧</a></a></p>
+
+    {{-- 現在の天気 --}}
     <div class="row text_center">
       <table class="table_weather now">
         <thead>
           <tr>
-            <th>2024/02/26(月) 21:00<br>現在の天気</th>
+            <th>{{ date('Y/m/d', $weatherNow['dt']) . '(' . $weatherNow['dt_day'] . ') ' . date('H:i') }}<br>現在の天気
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
+            <td><img src="http://openweathermap.org/img/wn/{{ $weatherNow['weather'][0]['icon'] }}@2x.png"
+                alt="{{ $weatherNow['weather'][0]['description'] }}"></td>
           </tr>
           <tr>
-            <td>小雨 6℃</td>
+            <td>{{ $weatherNow['weather'][0]['description'] }} {{ round($weatherNow['main']['temp']) }}°C
+            </td>
           </tr>
           <tr>
-            <td><span class="text_small">降水量:</span> 0.2<span class="text_small">mm</span></td>
+            <td>
+              <span class="text_small">降水量: </span>
+              @if (isset($weatherNow['rain']))
+                {{ $weatherNow['rain']['3h'] }}<span class="text_small">mm</span>
+              @elseif (isset($weatherNow['snow']))
+                {{ $weatherNow['snow']['3h'] }}<span class="text_small">mm</span>
+              @else
+                0.0<span class="text_small">mm</span>
+              @endif
+            </td>
           </tr>
           <tr>
-            <td><span class="text_small">風速:</span> <span class="text_small">北東</span> 6.3<span class="text_small">m/s</span></td>
+            <td><span class="text_small">風速: </span><span
+                class="text_small">{{ $weatherNow['wind']['direction'] }}</span>
+              {{ round($weatherNow['wind']['speed'], 1) }}<span class="text_small">m/s</span></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="row text_center pc_only">
-      <table class="table_weather pc_only">
-        <thead>
-          <tr>
-            <th colspan="9">今日 02/26(火)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th width="12%;">時刻</th>
-            <td width="11%;">00:00</td>
-            <td width="11%;">03:00</td>
-            <td width="11%;">06:00</td>
-            <td width="11%;">09:00</td>
-            <td width="11%;">12:00</td>
-            <td width="11%;">15:00</td>
-            <td width="11%;">18:00</td>
-            <td width="11%;">21:00</td>
-          </tr>
-          <tr>
-            <th></th>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-          </tr>
-          <tr>
-            <th>天気</th>
-            <td>小雨</td>
-            <td>小雨</td>
-            <td>小雨</td>
-            <td>小雨</td>
-            <td>小雨</td>
-            <td>小雨</td>
-            <td>小雨</td>
-            <td>小雨</td>
-          </tr>
-          <tr>
-            <th>気温</th>
-            <td>6℃</td>
-            <td>6℃</td>
-            <td>6℃</td>
-            <td>6℃</td>
-            <td>6℃</td>
-            <td>6℃</td>
-            <td>6℃</td>
-            <td>6℃</td>
-          </tr>
-          <tr>
-            <th>降水量</th>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td>0.2<span class="text_small">mm</span></td>
-          </tr>
-          <tr>
-            <th>風速</th>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="row text_center sp_only">
-      <table class="table_weather sp_only">
-        <thead>
-          <tr>
-            <th colspan="9">今日 02/27(火)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th width="20%;">時刻</th>
-            <th width="20%;"></th>
-            <th width="20%;">天気</th>
-            <th width="20%;">気温</th>
-            <th width="20%;">降水量</th>
-            <th width="20%;">風速</th>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-          <tr>
-            <td>00:00</td>
-            <td><img src="http://openweathermap.org/img/wn/10n@2x.png" alt="小雨"></td>
-            <td>小雨</td>
-            <td>6℃</td>
-            <td>0.2<span class="text_small">mm</span></td>
-            <td><span class="text_small">北東</span><br>6.3<span class="text_small">m/s</span></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    {{-- レイアウト調整用ここまで --}}
 
-    {{-- <div class="weather_wrap">
-      <ul class="list_unstyled">
-        @foreach ($weatherData['list'] as $i => $weather_3hours)
-          @php
-            $week = ['日', '月', '火', '水', '木', '金', '土'];
-            $date = date('w', $weather_3hours['dt']);
-          @endphp
-          <li>{{ date('Y/m/d H:i', $weather_3hours['dt']) . '(' . $week[$date] . ')' }}</li>
-          <li><img src="http://openweathermap.org/img/wn/{{ $weather_3hours['weather'][0]['icon'] }}@2x.png"
-              alt="{{ $weather_3hours['weather'][0]['description'] }}"></li>
-          <li>天気 : {{ $weather_3hours['weather'][0]['description'] }}</li>
-          <li>気温 : {{ round($weather_3hours['main']['temp']) }}°C</li>
-          @if (isset($weather_3hours['rain']))
-            <li>降水量 : {{ $weather_3hours['rain']['3h'] }}mm</li>
-          @else
-            <li>降水量: 0.0mm</li>
-          @endif
-          <li>風速 : {{ $degData[$i] . ' ' . round($weather_3hours['wind']['speed'], 1) }}m/s</li>
-        @endforeach
-      </ul>
-    </div> --}}
+    {{-- 3時間毎の天気/PC --}}
+    @foreach ($weather3hData as $i => $weather3hs)
+      <div class="row text_center pc_only">
+        <table class="table_weather">
+          <thead>
+            <tr>
+              <th colspan="9">{{ $i === 0 ? '今日 ' : '' }}{{ $i === 1 ? '明日 ' : '' }}{{ date('m/d', $weather3hs[$i]['dt']) . '(' . $weather3hs[$i]['dt_day'] . ')' }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>時刻</th>
+              {!! $i === array_key_first($weather3hData) ? $emptyTags['first'] : '' !!}
+              @foreach ($weather3hs as $weather3h)
+                <td>{{ date('H:i', $weather3h['dt']) }}</td>
+              @endforeach
+              {!! $i === array_key_last($weather3hData) ? $emptyTags['last'] : '' !!}
+            </tr>
+            <tr>
+              <th></th>
+              {!! $i === array_key_first($weather3hData) ? $emptyTags['first'] : '' !!}
+              @foreach ($weather3hs as $weather3h)
+                <td><img src="http://openweathermap.org/img/wn/{{ $weather3h['weather'][0]['icon'] }}@2x.png" alt="{{ $weather3h['weather'][0]['description'] }}"></td>
+              @endforeach
+              {!! $i === array_key_last($weather3hData) ? $emptyTags['last'] : '' !!}
+            </tr>
+            <tr>
+              <th>天気</th>
+              {!! $i === array_key_first($weather3hData) ? $emptyTags['first'] : '' !!}
+              @foreach ($weather3hs as $weather3h)
+                <td>{{ $weather3h['weather'][0]['description'] }}</td>
+              @endforeach
+              {!! $i === array_key_last($weather3hData) ? $emptyTags['last'] : '' !!}
+            </tr>
+            <tr>
+              <th>気温</th>
+              {!! $i === array_key_first($weather3hData) ? $emptyTags['first'] : '' !!}
+              @foreach ($weather3hs as $weather3h)
+                <td>{{ round($weather3h['main']['temp']) }}℃</td>
+              @endforeach
+              {!! $i === array_key_last($weather3hData) ? $emptyTags['last'] : '' !!}
+            </tr>
+            <tr>
+              <th>降水量</th>
+              {!! $i === array_key_first($weather3hData) ? $emptyTags['first'] : '' !!}
+              @foreach ($weather3hs as $weather3h)
+                <td>
+                  @if (isset($weather3h['rain']))
+                    {{ $weather3h['rain']['3h'] }}<span class="text_small">mm</span>
+                  @elseif (isset($weather3h['snow']))
+                    {{ $weather3h['snow']['3h'] }}<span class="text_small">mm</span>
+                  @else
+                    0.0<span class="text_small">mm</span>
+                  @endif
+                </td>
+              @endforeach
+              {!! $i === array_key_last($weather3hData) ? $emptyTags['last'] : '' !!}
+            </tr>
+            <tr>
+              <th>風速</th>
+              {!! $i === array_key_first($weather3hData) ? $emptyTags['first'] : '' !!}
+              @foreach ($weather3hs as $weather3h)
+                <td><span class="text_small">{{ $weather3h['wind']['direction'] }}</span><br>{{ round($weather3h['wind']['speed'], 1) }}<span class="text_small">m/s</span></td>
+              @endforeach
+              {!! $i === array_key_last($weather3hData) ? $emptyTags['last'] : '' !!}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    @endforeach
+    
   </div>
 </x-layout>
