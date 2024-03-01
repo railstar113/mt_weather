edc7b56e11e66d83cbc0a0cf66be6d6f2fb61b8f
railstar113 <kodama113@gmail.com>
2024-03-01 17:03:44 +0900

    run-apache2.shにドキュメントルート指定の設定追加

--- docker/app/run-apache2.sh
+++ docker/app/run-apache2.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sed -ri -e "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
+sed -ri -e "182a\DocumentRoot ${APACHE_DOCUMENT_ROOT}" /etc/apache2/apache2.conf
 rm /etc/apache2/mods-enabled/mpm_event.conf
 rm /etc/apache2/mods-enabled/mpm_event.load
 apache2-foreground "$@"
