2da6e956bac2b01d330bb95fe75b88c3af7e0d5d
railstar113 <kodama113@gmail.com>
2024-03-01 16:34:33 +0900

    run-apache2.shの修正

--- docker/app/run-apache2.sh
+++ docker/app/run-apache2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -ri "s/Listen 80/Listen ${APP_PORT:-80}/g" /etc/apache2/ports.conf
+sed -ri -e "s/Listen 80/Listen ${APP_PORT:-80}/g" /etc/apache2/ports.conf
 rm /etc/apache2/mods-enabled/mpm_event.conf
 rm /etc/apache2/mods-enabled/mpm_event.load
 apache2-foreground "$@"
